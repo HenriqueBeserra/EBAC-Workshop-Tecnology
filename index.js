593837127b8f329e64312530b863ecc3cb657ceb
HenriqueBeserra <millerhiquinho@gmail.com>
2022-06-01 21:16:02 -0300

    pokedex

---
+++ index.js
@@ -0,0 +1,33 @@
+const page = document.querySelector("#pokedex-page")
+
+function getPokemon () {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=150&offset=0")
+        .then( async response => {
+            return response.json();
+        })
+        .then(async data => {
+        
+            const box = document.querySelector("#pokemon-box");
+            
+           for(let i = 0; i < data.results.length; i++){
+                box.querySelector("#pokemon-name").innerHTML = data.results[i].name; //pegando os nomes dos pokemons na api
+                box.querySelector("#pokemon-name").style.textTransform = "capitalize";
+                
+                const pokemonImage = await fetch('https://pokeapi.co/api/v2/pokemon-form/' + data.results[i].name);
+                const image = await pokemonImage.json()
+                
+                box.querySelector("#pokemon-img").src = image.sprites.front_default
+                
+                page.innerHTML += box.outerHTML; //pegando os nomes da api e injetando no html
+           }
+            
+        })
+}
+    
+// async function getPokemon2 () {
+//     let pokemon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150&offset=0")
+//     poke = await pokemon.json()
+//     console.log(poke.results)
+// }
+
+getPokemon()
